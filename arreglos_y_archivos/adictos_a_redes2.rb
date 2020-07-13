147cc9bb8f4f0275847155cf43e4518a102bb528
fhernandez <franciscohernandez@gmail.com>
2020-07-13 17:53:50 -0400

    adictos_a_redes2.rb

---
+++ arreglos_y_archivos/adictos_a_redes2.rb
@@ -0,0 +1,20 @@
+
+
+def scan_addicts(array)
+    results = []
+    n = array.count
+    n.times do |i|
+        if array[i] <= 90
+            results.push 'bien'
+        elsif array[i] < 90 || array[i] > 180
+            results.push 'mejorable'
+        elsif array[i] >= 180
+            results.push 'mal'
+        end
+    end
+    results    
+end
+
+print scan_addicts([120, 50, 600, 30, 90, 10, 200, 0, 500])
+
+puts
