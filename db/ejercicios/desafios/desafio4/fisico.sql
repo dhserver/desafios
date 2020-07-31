7f98a6f771768c30750f2b10dc0422b182c634c3
fhernandez <franciscohernandez@gmail.com>
2020-07-31 00:32:47 -0400

    sql

---
+++ db/ejercicios/desafios/desafio4/fisico.sql
@@ -0,0 +1,34 @@
+CREATE TABLE autor(
+    id_autor INT PRIMARY KEY,
+    nombre_auto VARCHAR(25)
+);
+
+CREATE TABLE libro(
+    id_libro INT PRIMARY KEY,
+    nombre_libro VARCHAR(50)
+);
+
+CREATE TABLE editorial(
+    id_editorial INT PRIMARY KEY,
+    nombre_editorial VARCHAR(50)
+);
+
+CREATE TABLE lector(
+    id_lector INT PRIMARY KEY,
+    nombre_lector VARCHAR(50)
+);
+
+CREATE TABLE Autorlibro(
+    id_autor INT REFERENCES autor(id_autor),
+    id_libro INT REFERENCES libro(id_libro)
+);
+
+CREATE TABLE LibroEditorial(
+    id_autor INT REFERENCES autor(id_autor),
+    id_editorial INT REFERENCES editorial(id_editorial)
+);
+
+CREATE TABLE prestamo(
+    id_lector INT REFERENCES lector(id_lector),
+    id_libro INT REFERENCES libro(id_libro)
+);
